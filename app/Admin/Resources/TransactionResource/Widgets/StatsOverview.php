d8d625d1cf15a7369ce315c52f337cf64bfa3f46
Aditya Kumar <contact@adityadev.in>
2025-06-03 21:35:01 +0530

    Add Transaction management features: implement create, edit, and list pages with statistics overview
    Signed-off-by: adityakdevin <contact@adityadev.in>

--- app/Admin/Resources/TransactionResource/Widgets/StatsOverview.php
+++ app/Admin/Resources/TransactionResource/Widgets/StatsOverview.php
@@ -2,9 +2,12 @@
 
 namespace App\Admin\Resources\TransactionResource\Widgets;
 
+use App\Enums\CategoryType;
+use App\Enums\Status;
 use App\Models\Transaction;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
@@ -13,29 +16,29 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Income', '₹'.number_format(
-                Transaction::where('type', 'income')->sum('amount')
+            Stat::make('Total Income', Number::currency(
+                Transaction::where('type', 'income')->sum('amount'), 'INR', 'en_IN'
             ))
                 ->description('All-time recorded income')
                 ->icon('heroicon-o-banknotes')
                 ->color('success'),
 
-            Stat::make('Total Expenses', '₹'.number_format(
-                Transaction::where('type', 'expense')->sum('amount')
+            Stat::make('Total Expenses', Number::currency(
+                Transaction::where('type', 'expense')->sum('amount'), 'INR', 'en_IN'
             ))
                 ->description('Total money spent')
                 ->icon('heroicon-o-arrow-trending-down')
                 ->color('danger'),
 
-            Stat::make('Total Investments', '₹'.number_format(
-                Transaction::where('type', 'investment')->sum('amount')
+            Stat::make('Total Investments', Number::currency(
+                Transaction::where('type', 'investment')->sum('amount'), 'INR', 'en_IN'
             ))
                 ->description('Total invested so far')
                 ->icon('heroicon-o-chart-bar')
                 ->color('info'),
 
-            Stat::make('Total Debts', '₹'.number_format(
-                Transaction::where('type', 'debt')->sum('amount')
+            Stat::make('Total Debts', Number::currency(
+                Transaction::where(['type' => CategoryType::DEBT, 'status' => Status::PENDING])->sum('amount'), 'INR', 'en_IN'
             ))
                 ->description('Loans, EMIs, Credit Dues')
                 ->icon('heroicon-o-credit-card')
